bf6f5b5e3baf3e5c0883928aab77e4965fab2879
yashu1412 <135001156+yashu1412@users.noreply.github.com>
2025-07-19 21:12:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const hashRoutes = require('./routes/hashRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:8080';
+const FRONTEND_URL = process.env.FRONTEND_URL || 'https://tech-at-play-dev-tool-git-main-yashpawaras-projects.vercel.app/';
 
 app.use(cors({
   origin: FRONTEND_URL,
